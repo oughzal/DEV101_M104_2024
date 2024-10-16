3a7a3807badff8ea803d79e6dca463ca393ee0c1
omar OUGHZAL <omar.oughzal@gmail.com>
2024-10-16 17:03:44 +0100

    commit

--- CSS/test.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>) {
-    println("Hello")
-    for (i in 1..10) {
-        println(i)
-        
-    }
-}
--- CSS/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "Hello";
-for($i=0;$i<10;$i++){
-    echo "item $i\n";
-}
-?>
